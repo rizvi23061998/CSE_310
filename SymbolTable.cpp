85ba99f57cf53e92c87cd7b7bba7eac72ad1c6c9
rizvi23061998 <rizvi23061998@gmail.com>
2018-04-10 20:32:37 +0600

    SymbolInfo Done

---
+++ SymbolTable.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+class SymbolInfo{
+private:
+    string name;
+    string type;
+public:
+    SymbolInfo *next;
+
+    void setName(string name){
+        this->name = name;
+    }
+    string getName(){
+        return name;
+    }
+    void setType(string type){
+        this->type = type;
+    }
+    string getType(){
+        return type;
+    }
+};
+
+
+class ScopeTable{
+private:
+    int n;
+
+
+
+
+};
+
+
+int main(){
+    SymbolInfo
+    return 0;
+}
